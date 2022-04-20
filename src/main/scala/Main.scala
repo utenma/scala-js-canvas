ee40aed6d41641a523f389359550639fad9ea05c
saulpalv <19735362+saulpalv@users.noreply.github.com>
2022-04-20 17:26:48 -0600

    dom manipulstion

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,33 @@ import org.scalajs.dom.document
 import scala.scalajs.js.annotation.JSExportTopLevel
 
 @main def hello: Unit =
+  document.addEventListener(
+    "DOMContentLoaded",
+    { (e: dom.Event) =>
+      setupUI()
+    }
+  )
+
+def setupUI(): Unit = {
+  val button = document.createElement("button")
+  button.textContent = "Click me!"
+  button.addEventListener(
+    "click",
+    { (e: dom.MouseEvent) =>
+      addClickedMessage()
+    }
+  )
+  document.body.appendChild(button)
+
   appendPar(document.body, "Hello World")
+}
+
+def addClickedMessage(): Unit = {
+  appendPar(document.body, "You clicked the button!")
+}
 
 def appendPar(targetNode: dom.Node, text: String): Unit = {
   val parNode = document.createElement("p")
   parNode.textContent = text
   targetNode.appendChild(parNode)
-}
-
-@JSExportTopLevel("addClickedMessage")
-def addClickedMessage(): Unit = {
-  appendPar(document.body, "You clicked the button!")
 }
